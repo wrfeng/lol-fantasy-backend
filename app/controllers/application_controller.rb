b5ba650ffd27d6f042e9590b8a94e58ef4fdff92
Wei <theweifeng@gmail.com>
2019-07-21 17:41:17 -0400

    set up jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  def encode_token(payload)
+    JWT.encode payload, 'choco123', 'HS256'
+  end
+
+  def user_payload(user)
+    { user_id: user.id}
+  end
+
+  def token
+    request.headers["Authorization"]
+  end
+
+  def decoded_token
+    JWT.decode token, secret, true, {algorithm: 'HS256'}
+  end
+
+  def current_user
+    User.find(decoded_token[0]["user_id"])
+  end
+
+  def secret 
+    Rails.application.credentials.app_secret
+  end
 end
+
+# payload = {user_id: user.id}
+# token = JWT.encode payload, 'choco123', 'HS256'
+# render json: {token: token}
+
+# token = request.headers["Authorization"]
+# decoded_token = JWT.decode token, 'choco123', true, {algorithm: 'HS256'}
+# user_id = decoded_token[0]["user_id"]
+# user = User.find(user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :players
-  resources :drafts
-  resources :matchups
-  resources :drafted_teams
-  resources :leagues
+  resources :users, only: [:index, :show]
+  resources :players, only: [:index, :show]
+  resources :drafts, only: [:index, :show]
+  resources :matchups, only: [:index, :show]
+  resources :drafted_teams, only: [:index, :show]
+  resources :leagues, only: [:index, :show]
+
+  post "/login", to: "auth#login"
+  post "/signup", to: "users#create"
+  get "/profile", to: "users#profile"
 end
